ed77c1c3c36ca71213c4babf03c58aa88e6a301c
choungion <leefuentespablo.1@hotmail.es>
2026-07-02 17:24:29 -0600

    Creacion de formato inicial

---
+++ src/main/java/com/taru/TaruApplication.java
@@ -0,0 +1,13 @@
+package com.taru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaruApplication.class, args);
+	}
+
+}
